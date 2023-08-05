12648c2e873cca79ceb0185c4228f6a0afbc5490
VishnuKMi <vishnuarley@gmail.com>
2023-08-05 14:14:56 +0530

    reconfig

--- hardhat-tutorial/constants/dist/index.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var WHITELIST_CONTRACT_ADDRESS = "0xAC1b16220aB97E8C678556E9de38179006Ec832c";
-var METADATA_URL = "https://nft-collection-tutorial.vercel.app/api/";
-module.exports = {
-  WHITELIST_CONTRACT_ADDRESS: WHITELIST_CONTRACT_ADDRESS,
-  METADATA_URL: METADATA_URL
-};
--- hardhat-tutorial/constants/index.js
+++
@@ -1,4 +0,0 @@
-const WHITELIST_CONTRACT_ADDRESS = "0xAC1b16220aB97E8C678556E9de38179006Ec832c";
-const METADATA_URL = "https://nft-collection-tutorial.vercel.app/api/";
-
-module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
